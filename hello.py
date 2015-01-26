54026615661f3ae5827a866f18e8cef634f66597
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-01-26 01:30:51 +0000

    Test routing

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World! #2'
+@app.route('/foo/<xyz>')
+def hello_world(xyz):
+    return 'Hello, ' + xyz
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
